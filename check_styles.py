e2b806932df4c3e07f2dbdd1110b5232b71fd489
koolo233 <koolo233@163.com>
2023-11-08 08:23:54 +0800

    Update check_styles.py

---
+++ check_styles.py
@@ -0,0 +1,37 @@
+import os
+from PIL import Image
+from tqdm import tqdm
+
+
+if __name__ == "__main__":
+    train_styles_folder = "./wikiart/train"
+    test_styles_folder = "./wikiart/test"
+    val_styles_folder = "./wikiart/validation"
+
+    # check all images
+    print("begin check training styles...")
+    for style_name in tqdm(os.listdir(train_styles_folder)):
+        image_path = os.path.join(train_styles_folder, style_name)
+        try:
+            img = Image.open(image_path).convert('RGB')
+        except:
+            print(image_path)
+            os.system(f"rm {image_path}")
+
+    print("begin check testing styles...")
+    for style_name in tqdm(os.listdir(test_styles_folder)):
+        image_path = os.path.join(test_styles_folder, style_name)
+        try:
+            img = Image.open(image_path).convert('RGB')
+        except:
+            print(image_path)
+            os.system(f"rm {image_path}")
+
+    print("begin check validation styles...")
+    for style_name in tqdm(os.listdir(val_styles_folder)):
+        image_path = os.path.join(val_styles_folder, style_name)
+        try:
+            img = Image.open(image_path).convert('RGB')
+        except:
+            print(image_path)
+            os.system(f"rm {image_path}")
